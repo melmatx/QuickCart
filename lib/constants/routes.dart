6a199d8758616ed94dc818c948d43a7d6b28b7eb
melmatx <melpalana13@gmail.com>
2023-04-21 20:04:20 +0800

    Fixed bug where you can return after log out

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -6,7 +6,7 @@ class Routes {
   Future<dynamic> pushAndRemoveUntil(
       {required Widget widget, required BuildContext context, bool routeNav = false}) {
     return Navigator.of(context, rootNavigator: routeNav).pushAndRemoveUntil(
-        MaterialPageRoute(builder: (ctx) => widget), (route) => routeNav);
+        MaterialPageRoute(builder: (ctx) => widget), (route) => false);
   }
 
   Future<dynamic> push(
