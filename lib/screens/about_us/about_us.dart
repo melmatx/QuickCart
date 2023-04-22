704ae9f7000b5781a649b35767147a2bef0add45
melmatx <melpalana13@gmail.com>
2023-04-23 00:59:01 +0800

    Changed about us text

--- lib/screens/about_us/about_us.dart
+++ lib/screens/about_us/about_us.dart
@@ -17,9 +17,9 @@ class AboutUs extends StatelessWidget {
         ),
       ),
       body: const Padding(
-        padding: EdgeInsets.all(12.0),
+        padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 15.0),
         child: Text(
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+            "Founded in 2023 by a team of four talented Computer Science students, Wyndelle Cordova, Clint Claro, Jeffrey Mamac, and Mel Mathew Palaña, QuickCart is an online ordering system that revolutionizes the way customers shop and purchase products online. As avid fans of online shopping, the researchers noticed that many online ordering systems were difficult to navigate, leading to frustration and wasted time for customers. Determined to create a solution, the four students came together to develop an online ordering system that was user-friendly, efficient, and fast. They envisioned a platform that would offer a wide range of products from various merchants, providing customers with a convenient and hassle-free shopping experience and thus QuickCart was born."),
       ),
     );
   }
