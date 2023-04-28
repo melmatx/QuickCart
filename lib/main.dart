149f7e1026ce434a95f1774d6cbb9ec56c0aeed7
melmatx <melpalana13@gmail.com>
2023-04-28 09:19:21 +0800

    Added onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
@@ -9,18 +10,36 @@ import 'package:quickcart/firebase_helper/firebase_auth_helper/firebase_auth_hel
 import 'package:quickcart/provider/app_provider.dart';
 import 'package:quickcart/screens/auth_ui/welcome/welcome.dart';
 import 'package:quickcart/screens/custom_bottom_bar/custom_bottom_bar.dart';
+import 'package:quickcart/screens/onboarding/onboarding.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'firebase_helper/firebase_options.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+int? isViewed;
 
 void main() async {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarBrightness: Brightness.dark,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+  
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Get Stripe publishable key
   Stripe.publishableKey =
       "pk_test_51MWx8OAVMyklfe3CsjEzA1CiiY0XBTlHYbZ8jQlGtVFIwQi4aNeGv8J1HUw4rgSavMTLzTwgn0XRlwoTVRFXyu2h00mRUeWmAf";
+
+  // Initialize Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Get onboarding status
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  // await prefs.setInt('onBoard', 1);
+  isViewed = prefs.getInt('onBoard');
 
   runApp(const MyApp());
 }
@@ -41,9 +60,14 @@ class MyApp extends StatelessWidget {
         home: StreamBuilder(
           stream: FirebaseAuthHelper.instance.getAuthChange,
           builder: (context, snapshot) {
+            if (isViewed != 0) {
+              return const OnBoarding();
+            }
+
             if (snapshot.hasData) {
               return const CustomBottomBar();
             }
+
             return const Welcome();
           },
         ),
---
+++ lib/screens/onboarding/onboarding.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../constants/asset_images.dart';
+import '../../constants/routes.dart';
+import '../../firebase_helper/firebase_auth_helper/firebase_auth_helper.dart';
+import '../auth_ui/welcome/welcome.dart';
+import '../custom_bottom_bar/custom_bottom_bar.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({super.key});
+
+  _storeOnboardInfo() async {
+    int isViewed = 0;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('onBoard', isViewed);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return OnBoardingSlider(
+      finishButtonText: 'Get Started',
+      onFinish: () {
+        _storeOnboardInfo();
+        Widget home = StreamBuilder(
+          stream: FirebaseAuthHelper.instance.getAuthChange,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const CustomBottomBar();
+            }
+
+            return const Welcome();
+          },
+        );
+        Routes.instance.pushAndRemoveUntil(widget: home, context: context);
+      },
+      finishButtonStyle: const FinishButtonStyle(
+        elevation: 20,
+        backgroundColor: Colors.red,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(25.0),
+          ),
+        ),
+      ),
+      skipTextButton: const Text(
+        'Skip',
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.red,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      controllerColor: Colors.red,
+      totalPage: 3,
+      headerBackgroundColor: Colors.white,
+      pageBackgroundColor: Colors.white,
+      background: [
+        Image.asset(
+          AssetsImages.instance.slide_1,
+          height: 400,
+        ),
+        Image.asset(
+          AssetsImages.instance.slide_2,
+          height: 400,
+        ),
+        Image.asset(
+          AssetsImages.instance.slide_3,
+          height: 400,
+        ),
+      ],
+      speed: 1.8,
+      pageBodies: [
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 480,
+              ),
+              Text(
+                'Welcome to QuickCart!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Your seamless shopping experience awaits.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 480,
+              ),
+              Text(
+                "Explore Our Selection",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Find your favorite products in just a few taps.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 480,
+              ),
+              Text(
+                'Fast & Secure Checkout',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Enjoy a hassle-free payment experience.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black26,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
