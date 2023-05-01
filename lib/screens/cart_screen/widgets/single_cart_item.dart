deacbe7a558d31e3cc421c85c2777924a2808a67
melmatx <melpalana13@gmail.com>
2023-05-01 21:16:51 +0800

    Made cart and favorite items clickable

--- lib/screens/cart_screen/widgets/single_cart_item.dart
+++ lib/screens/cart_screen/widgets/single_cart_item.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quickcart/models/product_model/product_model.dart';
 import 'package:quickcart/provider/app_provider.dart';
+import 'package:quickcart/screens/product_details/product_details.dart';
 
 import '../../../constants/constants.dart';
+import '../../../constants/routes.dart';
 
 class SingleCartItem extends StatefulWidget {
   final ProductModel singleProduct;
@@ -21,9 +23,11 @@ class _SingleCartItemState extends State<SingleCartItem> {
     AppProvider appProvider = Provider.of<AppProvider>(
       context,
     );
+    
     int qty = appProvider.getQty(widget.singleProduct);
     widget.singleProduct.isFavourite = appProvider.getFavouriteProductList
         .any((element) => element.id == widget.singleProduct.id);
+
     return Container(
       margin: const EdgeInsets.only(bottom: 12.0),
       decoration: BoxDecoration(
@@ -32,152 +36,160 @@ class _SingleCartItemState extends State<SingleCartItem> {
         ),
         border: Border.all(color: Theme.of(context).primaryColor, width: 3),
       ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(6),
-              height: 140,
-              width: 120,
-              color: Theme.of(context).primaryColor.withOpacity(0.5),
-              child: Image.network(
-                widget.singleProduct.image,
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: SizedBox(
-              height: 140,
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Stack(
-                  alignment: Alignment.bottomRight,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 165,
-                              child: Text(
-                                widget.singleProduct.name,
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 7,
-                            ),
-                            Row(
-                              children: [
-                                CupertinoButton(
-                                  onPressed: () {
-                                    if (qty > 1) {
-                                      setState(() {
-                                        qty--;
-                                      });
-                                      appProvider.updateQty(
-                                          widget.singleProduct, qty);
-                                    } else {
-                                      appProvider.removeCartProduct(
-                                          widget.singleProduct);
-                                      showMessage("Removed from Cart");
-                                    }
-                                  },
-                                  padding: EdgeInsets.zero,
-                                  child: const CircleAvatar(
-                                    maxRadius: 13,
-                                    child: Icon(Icons.remove),
-                                  ),
-                                ),
-                                Text(
-                                  qty.toString(),
-                                  style: const TextStyle(
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                CupertinoButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      qty++;
-                                    });
-                                    appProvider.updateQty(
-                                        widget.singleProduct, qty);
-                                  },
-                                  padding: EdgeInsets.zero,
-                                  child: const CircleAvatar(
-                                    maxRadius: 13,
-                                    child: Icon(Icons.add),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            CupertinoButton(
-                              padding: EdgeInsets.zero,
-                              onPressed: () {
-                                if (!widget.singleProduct.isFavourite) {
-                                  appProvider.addFavouriteProduct(
-                                      widget.singleProduct);
-                                  showMessage("Added to wishlist");
-                                } else {
-                                  appProvider.removeFavouriteProduct(
-                                      widget.singleProduct);
-                                  showMessage("Removed from wishlist");
-                                }
-                              },
-                              child: Text(
-                                widget.singleProduct.isFavourite
-                                    ? "Remove from wishlist"
-                                    : "Add to wishlist",
-                                style: const TextStyle(
-                                  fontSize: 12.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          width: 55,
-                          child: AutoSizeText(
-                            textAlign: TextAlign.right,
-                            "₱${widget.singleProduct.price % 1 == 0 ? widget.singleProduct.price.round().toString() : widget.singleProduct.price.toStringAsFixed(2)}",
-                            style: const TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            maxLines: 1,
-                          ),
-                        ),
-                      ],
-                    ),
-                    CupertinoButton(
-                        padding: EdgeInsets.zero,
-                        onPressed: () {
-                          appProvider.removeCartProduct(widget.singleProduct);
-                          showMessage("Removed from Cart");
-                        },
-                        child: const CircleAvatar(
-                          maxRadius: 13,
-                          child: Icon(
-                            Icons.delete,
-                            size: 17,
-                          ),
-                        )),
-                  ],
+      child: InkWell(
+        onTap: () {
+          Routes.instance.push(
+              widget: ProductDetails(singleProduct: widget.singleProduct),
+              context: context);
+        },
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(6),
+                height: 140,
+                width: 120,
+                color: Theme.of(context).primaryColor.withOpacity(0.5),
+                child: Image.network(
+                  widget.singleProduct.image,
                 ),
               ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 2,
+              child: SizedBox(
+                height: 140,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.38,
+                                child: AutoSizeText(
+                                  widget.singleProduct.name,
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Row(
+                                children: [
+                                  CupertinoButton(
+                                    onPressed: () {
+                                      if (qty > 1) {
+                                        setState(() {
+                                          qty--;
+                                        });
+                                        appProvider.updateQty(
+                                            widget.singleProduct, qty);
+                                      } else {
+                                        appProvider.removeCartProduct(
+                                            widget.singleProduct);
+                                        showMessage("Removed from Cart");
+                                      }
+                                    },
+                                    padding: EdgeInsets.zero,
+                                    child: const CircleAvatar(
+                                      maxRadius: 13,
+                                      child: Icon(Icons.remove),
+                                    ),
+                                  ),
+                                  Text(
+                                    qty.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  CupertinoButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        qty++;
+                                      });
+                                      appProvider.updateQty(
+                                          widget.singleProduct, qty);
+                                    },
+                                    padding: EdgeInsets.zero,
+                                    child: const CircleAvatar(
+                                      maxRadius: 13,
+                                      child: Icon(Icons.add),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              CupertinoButton(
+                                padding:
+                                    const EdgeInsets.only(left: 3.0, top: 3.0),
+                                onPressed: () {
+                                  if (!widget.singleProduct.isFavourite) {
+                                    appProvider.addFavouriteProduct(
+                                        widget.singleProduct);
+                                    showMessage("Added to wishlist");
+                                  } else {
+                                    appProvider.removeFavouriteProduct(
+                                        widget.singleProduct);
+                                    showMessage("Removed from wishlist");
+                                  }
+                                },
+                                child: Text(
+                                  widget.singleProduct.isFavourite
+                                      ? "Remove from wishlist"
+                                      : "Add to wishlist",
+                                  style: const TextStyle(
+                                    fontSize: 12.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 55,
+                            child: AutoSizeText(
+                              textAlign: TextAlign.right,
+                              "₱${widget.singleProduct.price % 1 == 0 ? widget.singleProduct.price.round().toString() : widget.singleProduct.price.toStringAsFixed(2)}",
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              maxLines: 1,
+                            ),
+                          ),
+                        ],
+                      ),
+                      CupertinoButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {
+                            appProvider.removeCartProduct(widget.singleProduct);
+                            showMessage("Removed from Cart");
+                          },
+                          child: const CircleAvatar(
+                            maxRadius: 13,
+                            child: Icon(
+                              Icons.delete,
+                              size: 17,
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/favourite_screen/widgets/single_favourite_item.dart
+++ lib/screens/favourite_screen/widgets/single_favourite_item.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quickcart/models/product_model/product_model.dart';
 import 'package:quickcart/provider/app_provider.dart';
+import 'package:quickcart/screens/product_details/product_details.dart';
 
 import '../../../constants/constants.dart';
+import '../../../constants/routes.dart';
 
 class SingleFavouriteItem extends StatefulWidget {
   final ProductModel singleProduct;
@@ -27,116 +29,125 @@ class _SingleFavouriteItemState extends State<SingleFavouriteItem> {
         ),
         border: Border.all(color: Theme.of(context).primaryColor, width: 3),
       ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(6),
-              height: 140,
-              color: Theme.of(context).primaryColor.withOpacity(0.5),
-              child: Image.network(
-                widget.singleProduct.image,
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: SizedBox(
-              height: 140,
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Stack(
-                  alignment: Alignment.bottomRight,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 165,
-                              child: Text(
-                                widget.singleProduct.name,
-                                style: const TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 6.0,
-                            ),
-                            CupertinoButton(
-                              padding: EdgeInsets.zero,
-                              onPressed: () {
-                                if (!appProvider.getCartProductList.any(
-                                    (element) =>
-                                        element.id ==
-                                        widget.singleProduct.id)) {
-                                  appProvider.addCartProduct(
-                                      widget.singleProduct.copyWith(qty: 1));
-                                  showMessage("Added to Cart");
-                                } else {
-                                  appProvider
-                                      .removeCartProduct(widget.singleProduct);
-                                  showMessage("Removed from Cart");
-                                }
-                              },
-                              child: Text(
-                                appProvider.getCartProductList.any((element) =>
-                                        element.id == widget.singleProduct.id)
-                                    ? "Remove from Cart"
-                                    : "Add to Cart",
-                                style: const TextStyle(
-                                  fontSize: 12.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            CupertinoButton(
-                              padding: EdgeInsets.zero,
-                              onPressed: () {
-                                AppProvider appProvider =
-                                    Provider.of<AppProvider>(context,
-                                        listen: false);
-                                appProvider.removeFavouriteProduct(
-                                    widget.singleProduct);
-                                showMessage("Removed from wishlist");
-                              },
-                              child: const Text(
-                                "Remove from wishlist",
-                                style: TextStyle(
-                                  fontSize: 12.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          width: 55,
-                          child: AutoSizeText(
-                            textAlign: TextAlign.right,
-                            "₱${widget.singleProduct.price % 1 == 0 ? widget.singleProduct.price.round().toString() : widget.singleProduct.price.toStringAsFixed(2)}",
-                            style: const TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            maxLines: 1,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+      child: InkWell(
+        onTap: () {
+          Routes.instance.push(
+              widget: ProductDetails(singleProduct: widget.singleProduct),
+              context: context);
+        },
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(6),
+                height: 140,
+                color: Theme.of(context).primaryColor.withOpacity(0.5),
+                child: Image.network(
+                  widget.singleProduct.image,
                 ),
               ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 2,
+              child: SizedBox(
+                height: 140,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.38,
+                                child: Text(
+                                  widget.singleProduct.name,
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 9,
+                              ),
+                              CupertinoButton(
+                                padding: EdgeInsets.zero,
+                                onPressed: () {
+                                  if (!appProvider.getCartProductList.any(
+                                      (element) =>
+                                          element.id ==
+                                          widget.singleProduct.id)) {
+                                    appProvider.addCartProduct(
+                                        widget.singleProduct.copyWith(qty: 1));
+                                    showMessage("Added to Cart");
+                                  } else {
+                                    appProvider.removeCartProduct(
+                                        widget.singleProduct);
+                                    showMessage("Removed from Cart");
+                                  }
+                                },
+                                child: Text(
+                                  appProvider.getCartProductList.any(
+                                          (element) =>
+                                              element.id ==
+                                              widget.singleProduct.id)
+                                      ? "Remove from Cart"
+                                      : "Add to Cart",
+                                  style: const TextStyle(
+                                    fontSize: 12.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              CupertinoButton(
+                                padding: EdgeInsets.zero,
+                                onPressed: () {
+                                  AppProvider appProvider =
+                                      Provider.of<AppProvider>(context,
+                                          listen: false);
+                                  appProvider.removeFavouriteProduct(
+                                      widget.singleProduct);
+                                  showMessage("Removed from wishlist");
+                                },
+                                child: const Text(
+                                  "Remove from wishlist",
+                                  style: TextStyle(
+                                    fontSize: 12.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 55,
+                            child: AutoSizeText(
+                              textAlign: TextAlign.right,
+                              "₱${widget.singleProduct.price % 1 == 0 ? widget.singleProduct.price.round().toString() : widget.singleProduct.price.toStringAsFixed(2)}",
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              maxLines: 1,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
