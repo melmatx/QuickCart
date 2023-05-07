e38b906f884cdca9f80f92172457883ff6a1a5c3
melmatx <melpalana13@gmail.com>
2023-05-08 00:40:50 +0800

    Renamed environment variable

--- lib/screens/chat_screen/chat_screen.dart
+++ lib/screens/chat_screen/chat_screen.dart
@@ -24,7 +24,7 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   void initState() {
     openAI = OpenAI.instance.build(
-        token: const String.fromEnvironment('API_KEY'),
+        token: const String.fromEnvironment('OPENAI_API_KEY'),
         baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)));
     super.initState();
   }
